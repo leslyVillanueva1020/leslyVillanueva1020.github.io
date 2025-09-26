45e97e1ff285fff4dce0478f11cee446e5733939
LeslyV <l3sly2022@gmail.com>
2025-09-25 21:17:24 -0700

    addes js functonality to all radio buttons

--- Hw2/script.js
+++ Hw2/script.js
@@ -1,10 +1,23 @@
 let scene = document.querySelector("#selectScene");
-let shirt = document.querySelector(".shirtOpt");
-let bottoms = document.querySelector("pantsOpt");
-let headwear = document.querySelector(".hat");
-let shoes = document.querySelector(".shoes")
+let shirt = document.querySelector("#shirtRadio");
+let hoodie = document.querySelector("#hoodieRadio");
+let jeans = document.querySelector("#jeansRadio");
+let skirt = document.querySelector("#skirtRadio");
+let beanie = document.querySelector("#beanieRadio");
+let headband = document.querySelector("#headbandRadio");
+let shoe = document.querySelector("#shoeRadio");
+let boot = document.querySelector("#bootRadio");
 
-scene.addEventListener("click", changeScene);
+
+scene.addEventListener("change", changeScene);
+shirt.addEventListener("change", changeShirt);
+hoodie.addEventListener("change", changeShirt);
+jeans.addEventListener("change" , changeBottoms);
+skirt.addEventListener("change", changeBottoms);
+beanie.addEventListener("change", changeHat);
+headband.addEventListener("change", changeHat);
+shoe.addEventListener("change", changeShoe);
+boot.addEventListener("change", changeShoe);
 
 
 function changeScene() {
@@ -21,5 +34,59 @@ function changeScene() {
     }
     else {
         previewBckgnd.style.backgroundImage = "url(imgs/bedroom.jpg)";
+    }
+}
+
+function changeShirt() {
+    let shirtType = document.querySelector("input[name=shirt]:checked").value;
+    let shirtImg = document.querySelector("#shirtImg");
+
+    if(shirtType == "shirt"){
+        shirtImg.src = "imgs/shirt.png";
+    }
+    else if(shirtType == "hoodie"){
+        shirtImg.src = "imgs/hoodie.png";
+    }
+}
+
+function changeBottoms() {
+    let bottomType = document.querySelector("input[name=pants]:checked").value;
+    let pantsImg = document.querySelector("#pantsImg");
+
+    if(bottomType == "jeans"){
+        pantsImg.src = "imgs/jeans.png";
+    } 
+    else if (bottomType == "skirt") {
+        pantsImg.src = "imgs/skirt.png";
+    }
+}
+
+function changeHat() {
+    let hatType = document.querySelector("input[name=hat]:checked").value;
+    let hatImg = document.querySelector("#hatImg");
+
+    if(hatType == "beanie"){
+        hatImg.src = "imgs/beanie.png";
+        hatImg.style.display = "block";
+    }
+    else if(hatType == "headBand") {
+        hatImg.src = "imgs/headband.png";
+        hatImg.style.display = "block";
+    }
+}
+
+function changeShoe(){
+    let shoeType = document.querySelector("input[name=shoe]:checked").value;
+    let shoesImg = document.querySelector("#shoesImg");
+
+    if(shoeType == "sneaker"){
+        shoesImg.src = "imgs/sneakers.png";
+    }
+    else if(shoeType == "boots") {
+        shoesImg.src = "imgs/boots.png";
+    }
+
+    if(document.querySelector("#pantsImg").src != "imgs/pjPants.png"){
+        shoesImg.style.zIndex = 3;
     }
 }
