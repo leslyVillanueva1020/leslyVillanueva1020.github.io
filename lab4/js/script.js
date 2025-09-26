2018a78a560a246016b9f26a0bf07152c02130cf
LeslyV <l3sly2022@gmail.com>
2025-09-25 20:07:04 -0700

    different forest photo

---
+++ lab4/js/script.js
@@ -0,0 +1,48 @@
+//Event Listeners
+document.querySelector("#zip").addEventListener("change", displayCity)
+
+displayStates();
+
+async function displayCity() {
+    let zipCode = document.querySelector("#zip").value;
+    let url = "https://csumb.space/api/cityInfoAPI.php?zip=" + zipCode;
+    try {
+        let response = await fetch(url);
+        try {
+            let data = await response.json();
+            //console.log(data);
+            document.querySelector("#city").textContent = data.city;
+        } catch (parseError) {
+            console.log("JSON Parsing error " + parseError);
+        }
+    } catch (error) {
+        console.log("Network error " + error);
+    }
+    //alert(zipCode)
+}
+
+
+async function displayStates() {
+    let url = "https://csumb.space/api/allStatesAPI.php";
+    try {
+        let response = await fetch(url);
+        try {
+            let data = await response.json();
+            console.log(data);
+
+            for (let i of data) {
+                let optionElement = document.createElement("option");
+                optionElement.textContent = i.state;
+                optionElement.value = i.usps;
+
+                document.querySelector("#state").append(optionElement);
+            }
+
+        } catch (parseError) {
+            console.log("JSON Parsing error " + parseError);
+        }
+    } catch (error) {
+        console.log("Network error " + error);
+    }
+    //alert(zipCode)
+}
