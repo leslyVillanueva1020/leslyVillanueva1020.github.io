1c1853c7911623ccd8c57959d4b1c27b336fc310
LeslyV <l3sly2022@gmail.com>
2025-09-27 10:39:36 -0700

    created for loop to show Q5 options check for correct answers written store user total score and times taken the quiz addedd styling to the website

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,46 +1,14 @@
-// document.querySelector("button").addEventListener("click", gradeQuiz);
-
-// displayQ3Options();
-// function displayQ3Options() {
-
-//     let q3Options = ["font-color", "fontColor", "color", "text-color"];
-//     q3Options = _.shuffle(q3Options);
-
-//     for(let i of q3Options){
-
-//         let inputElement = document.createElement("input");
-//         inputElement.type = "radio";
-//         inputElement.name = "q3";
-//         inputElement.value = i;
-//         console.log(inputElement);
-
-//         let labelElement = document.createElement("label");
-//         labelElement.textContent = i;
-//         labelElement.prepend(inputElement);
-
-//         document.querySelector("#q3Options").append(labelElement);
-//     }
-
-// }
-
-// function gradeQuiz() {
-//     let userAnswer1 = document.querySelector("input[name=q1]:checked").value;
-//     // alert(userAnswer1);
-
-//     if(userAnswer1 == "color") {
-//         alert("Right!");
-//     } else {
-//         alert("Wrong!");
-//     }
-// }
-
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
 displayQ1Options();
+displayQ5Options();
+
 function displayQ1Options() {
     let q1Options = ["Phoenix", "New York", "Austin", "Toronto"];
 
-    for (let i of q1Options) {
+    let shuffledOptions = _.shuffle(q1Options);
+
+    for (let i of shuffledOptions) {
         let inputElement = document.createElement("input");
         inputElement.type = "radio";
         inputElement.name = "q1";
@@ -55,51 +23,160 @@ function displayQ1Options() {
     }
 }
 
+function displayQ5Options(){
+    let q5Options = ["Las Vegas", "Sacramento", "Seattle", "Albany", "Honolulu"]
+
+    for(let i of q5Options) {
+        let inputElement = document.createElement("input");
+        inputElement.type = "checkbox";
+        inputElement.name = "q5";
+        inputElement.value = i;
+        console.log(inputElement);
+
+        let labelElement = document.createElement("label");
+        labelElement.textContent = i;
+        labelElement.prepend(inputElement);
+
+        document.querySelector("#q5Options").append(labelElement);
+    }
+}
+
 function gradeQuiz() {
     let userAnswer1 = document.querySelector("input[name=q1]:checked").value;
     let userAnswer2 = document.querySelector("#popularCapt").value;
     let userAnswer3 = document.querySelector("#textInput").value;
     let userAnswer4 = document.querySelector("#numCapt").value;
+    let q5Input = document.querySelectorAll("input[name=q5]");
+    let correctAnsw5 = ["Sacramento", "Albany", "Honolulu"];
+    let totalScore = 0;
     
+
     if (userAnswer1 == "Phoenix") {
-        let afterQuiz = document.querySelector("#afterQuiz");
+        let afterQuiz = document.querySelector("#q1Msg");
         afterQuiz.textContent = "Correct";
         afterQuiz.style.color = "green";
+
+        document.querySelector("#q1Img").src = "img/checkmark.png";
+        document.querySelector("#q1Img").style.display = "inline";
+        totalScore += 20;
     }
     else {
-        let afterQuiz = document.querySelector("#afterQuiz");
+        let afterQuiz = document.querySelector("#q1Msg");
         afterQuiz.textContent = "Incorrect";
         afterQuiz.style.color = "red";
+        document.querySelector("#q1Img").src = "img/xmark.png";
+        document.querySelector("#q1Img").style.display = "inline";
     }
 
     if(userAnswer2 == "Washington D.C"){
-        let afterQuiz2 = document.querySelector("#afterQuiz2");
+        let afterQuiz2 = document.querySelector("#q2Msg");
         afterQuiz2.textContent = "Correct";
         afterQuiz2.style.color = "green";
+
+        document.querySelector("#q2Img").src = "img/checkmark.png";
+        document.querySelector("#q2Img").style.display = "inline";
+        totalScore += 20;
     } else {
-        let afterQuiz2 = document.querySelector("#afterQuiz2");
+        let afterQuiz2 = document.querySelector("#q2Msg");
         afterQuiz2.textContent = "Incorrect";
         afterQuiz2.style.color = "red";
+
+        document.querySelector("#q2Img").src = "img/xmark.png";
+        document.querySelector("#q2Img").style.display = "inline";
     }
 
-    if(userAnswer3 == "Cheyenne"){
-        let afterQuiz3 = document.querySelector("#afterQuiz3");
+    if(userAnswer3 == "cheyenne"){
+        let afterQuiz3 = document.querySelector("#q3Msg");
         afterQuiz3.textContent = "Correct";
         afterQuiz3.style.color = "green";
+
+        document.querySelector("#q3Img").src = "img/checkmark.png";
+        document.querySelector("#q3Img").style.display = "inline";
+        totalScore += 20;
     }
     else {
-        let afterQuiz3 = document.querySelector("#afterQuiz3");
+        let afterQuiz3 = document.querySelector("#q3Msg");
         afterQuiz3.textContent = "Incorrect";
         afterQuiz3.style.color = "red";
+
+        document.querySelector("#q3Img").src = "img/xmark.png";
+        document.querySelector("#q3Img").style.display = "inline";
     }
+
     if (userAnswer4 == "51") {
-        let afterQuiz4 = document.querySelector("#afterQuiz4");
+        let afterQuiz4 = document.querySelector("#q4Msg");
         afterQuiz4.textContent = "Correct";
         afterQuiz4.style.color = "green";
+
+        document.querySelector("#q4Img").src = "img/checkmark.png";
+        document.querySelector("#q4Img").style.display = "inline";
+        totalScore += 20;
     }
     else {
-        let afterQuiz4 = document.querySelector("#afterQuiz4");
+        let afterQuiz4 = document.querySelector("#q4Msg");
         afterQuiz4.textContent = "Incorrect";
         afterQuiz4.style.color = "red";
+
+        document.querySelector("#q4Img").src = "img/xmark.png";
+        document.querySelector("#q4Img").style.display = "inline";
     }
+
+    
+    let correctCount = 0;
+    let wrongChoice = false;
+
+    for(let i of q5Input) {
+        if(i.checked){
+            if(correctAnsw5.includes(i.value)){
+                correctCount++;
+            }
+            else {
+                wrongChoice = true;
+            }
+        }
+    }
+
+    let afterQuiz5 = document.querySelector("#q5Msg");
+    if(!wrongChoice && correctCount === correctAnsw5.length) {
+        afterQuiz5.textContent = "Correct";
+        afterQuiz5.style.color = "green";
+        
+        document.querySelector("#q5Img").src = "img/checkmark.png";
+        document.querySelector("#q5Img").style.display = "inline";
+        totalScore += 20;
+    } 
+    else {
+        afterQuiz5.textContent = "Incorrect";
+        afterQuiz5.style.color = "red";   
+        document.querySelector("#q5Img").src = "img/xmark.png";
+        document.querySelector("#q5Img").style.display = "inline";
+    }
+
+    let totalScoreDiv = document.querySelector("#totalScore");
+    totalScoreDiv.textContent = `Total Score: ${totalScore} / 100`;
+
+    congratsDiv = document.querySelector("#congratsMsg");
+    if(totalScore > 80) {
+        congratsDiv.textContent = "🎉 Great job! You scored above 80!";
+        congratsDiv.style.color = "green";
+    }
+    else {
+        congratsDiv.textContent = "";
+    }
+
+    //getting the value saved at local storage key(quiz times taken)
+    let times =  localStorage.getItem("quizTimesTaken");
+
+    //if test has not been done before instead of null we want 0 
+    if(times == null){
+        times = 0;
+    }
+    else {
+        //local storage give back a string so we have to turn it into an int
+        times = parseInt(times);
+    }
+
+    times += 1;
+    localStorage.setItem("quizTimesTaken", times);
+    document.querySelector("#timesTaken").textContent = "Times Taken: " + times;
 }
